bae9f672e97ebd613bdfb19158ff8b7c31bb7625
Jozhué Beni <jbeeniif@gmail.com>
2024-02-06 10:46:30 -0300

    Vercel File

--- src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-const app_1 = require("./app");
-const port = 3000;
-const prisma = new client_1.PrismaClient({ log: ["info", "query", "warn", "error"] });
-exports.prisma = prisma;
-app_1.app.listen(port, () => console.log(`Server is running in: http://localhost:${port}`));
